1df7de3cd76c7e237000dc39b18813b1efdb3451
Robin Kristiansson <robin.kristiansson@aller.com>
2019-11-21 13:23:34 +0100

    Chat functionality + game logic

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,22 @@ import './scss/style.scss';
 var client = new Colyseus.Client('ws://localhost:2567');
 var room;
 
-client.join("room_name").then(room => {
+client.joinOrCreate("tictactoe").then(room => {
   console.log(room.sessionId, "joined", room.name);
 
   room.onStateChange((state) => {
     console.log(room.name, "has new state:", state);
   });
 
-  room.onMessage((message) => {
-    console.log(client.id, "received on", room.name, message);
+  room.onMessage((data) => {
+    console.log(client.id, "received on", room.name, data);
+
+
+    if(data.action === 'chat') {
+      var p = document.createElement("p");
+      p.innerHTML = `${data.playerSessionId}: ${data.message}`;
+      document.querySelector("#messages").appendChild(p);
+    }
   });
 
   room.onError(() => {
@@ -22,7 +29,20 @@ client.join("room_name").then(room => {
 
   room.onLeave(() => {
     console.log(client.id, "left", room.name);
-});
+  });
+
+  document.querySelector("#form").onsubmit = function(e) {
+    e.preventDefault();
+    var input = document.querySelector("#input");
+    console.log("input:", input.value);
+    // send data to room
+    room.send({ 
+      action: 'chat',
+      message: input.value 
+    });
+    // clear input
+    input.value = "";
+  }
       
 }).catch(e => {
     console.log("JOIN ERROR", e);
