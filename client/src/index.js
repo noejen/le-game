284b0d451edf7ff9e7a1bdbb7a1eda29fec998e4
Jimmi Elofsson <jimmi.elofsson@aller.com>
2019-11-21 14:50:23 +0100

    Set state and remove calculateWinner

--- client/src/index.js
+++ client/src/index.js
@@ -135,7 +135,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = { name: null };
+    const winner = { name: null, squares: [] };
 
     const moves = history.map((step, move) => {
       const desc = move ?
